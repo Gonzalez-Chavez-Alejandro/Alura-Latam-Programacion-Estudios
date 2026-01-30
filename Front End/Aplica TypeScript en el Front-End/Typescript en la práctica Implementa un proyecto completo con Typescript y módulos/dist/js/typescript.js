14b0de049d67fabeecea56fc45cffa66d838ddc0
Wolf <a17jul@gmail.com>
2026-01-30 14:21:02 -0600

    Arrays y Type alias

--- Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/dist/js/typescript.js
+++ Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/dist/js/typescript.js
@@ -4,3 +4,11 @@ let nombre = "Juan";
 let isPago = true;
 let cualquiera = "22";
 cualquiera = "comida";
+const lista = [];
+//arrays
+lista.push(22, 55, 22, 44, 66);
+const nuevaTransaccion = {
+    tipoTransaccion: "",
+    fecha: new Date(),
+    valor: 0,
+};
--- Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/src/typescript.ts
+++ Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/src/typescript.ts
@@ -10,3 +10,22 @@ let cualquiera:any="22";
 
 cualquiera="comida";
 
+const lista:number[]=[];
+
+//arrays
+
+lista.push(22, 55, 22, 44, 66);
+ 
+//Tipos personalizados (typebank.ts)
+//Restringir los tipos de datos y que nombres espesificos deben de tener las propiedades de un objeto.
+type Transaccion={
+    tipoTransaccion:string,
+    fecha:Date,
+    valor:number
+}
+
+const nuevaTransaccion :Transaccion = {
+    tipoTransaccion:"",
+    fecha:new Date(),
+    valor:0,
+}
