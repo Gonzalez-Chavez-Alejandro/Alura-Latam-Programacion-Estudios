3a81b8cf072f3c7cab134a2d8db0d9f1e8a8f215
Wolf <a17jul@gmail.com>
2026-01-30 13:16:52 -0600

    Tipos primitivos

---
+++ Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/dist/js/typescript.js
@@ -0,0 +1,6 @@
+let valor = 3000;
+valor = 22;
+let nombre = "Juan";
+let isPago = true;
+let cualquiera = "22";
+cualquiera = "comida";
---
+++ Front End/Aplica TypeScript en el Front-End/Typescript en la práctica Implementa un proyecto completo con Typescript y módulos/src/typescript.ts
@@ -0,0 +1,12 @@
+let valor =3000;
+
+valor=22;
+
+let nombre:string="Juan";
+
+let isPago:boolean=true;
+
+let cualquiera:any="22";
+
+cualquiera="comida";
+
