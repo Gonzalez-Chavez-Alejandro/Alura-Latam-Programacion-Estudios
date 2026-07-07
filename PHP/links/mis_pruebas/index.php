62742b563bf8a1801100a6017cb8255e64b95e03
Wolf <a17jul@gmail.com>
2026-07-07 12:46:29 -0600

    Culturama

---
+++ PHP/links/mis_pruebas/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba</title>
+</head>
+<body>
+    <?php
+$numero = (int) 10.5;
+$string ="-";
+
+echo $string;
+
+echo "El número es: " . $numero;
+?>
+</body>
+</html>
